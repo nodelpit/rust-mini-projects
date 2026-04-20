2d970cf8a17dacd33ac19af5500d729eb251874f
Noah Delpit <noah02delpit@gmail.com>
2026-04-20 18:17:28 +0200

    terminal-rpg - Ticket 3: Add Fighter methods and apply_action function
    - Add 3 methods to Fighter: take_damage, heal and is_alive
    - Add apply_action function to resolve the 4 enum actions for hero and monster

--- terminal-rpg/src/main.rs
+++ terminal-rpg/src/main.rs
@@ -39,19 +39,66 @@ impl Fighter {
             max_hp,
         }
     }
+
+    fn take_damage(&mut self, amount: u16) {
+        self.current_hp = self.current_hp.saturating_sub(amount);
+    }
+
+    fn heal(&mut self, amount: u16) {
+        self.current_hp = (self.current_hp + amount).min(self.max_hp);
+    }
+
+    fn is_alive(&self) -> bool {
+        self.current_hp > 0
+    }
+}
+
+fn apply_action(hero: &mut Fighter, monster: &mut Fighter, action: Action) -> String {
+    match action {
+        Action::Attack => {
+            monster.take_damage(hero.attack_power.into());
+            "You attacked a monster".to_string()
+        }
+
+        Action::Defend => {
+            // TODO ticket 6
+            "You defended yourself of the monster".to_string()
+        }
+
+        Action::Heal => {
+            hero.heal(15);
+            "You healed yourself".to_string()
+        }
+
+        Action::Quit => {
+            "See you in another hell..".to_string()
+        }
+    }
 }
 
 fn main() {
-    let hero = Fighter::new("Rustler", 12, 100, 100);
-    let monster = Fighter::new("Pythor", 15, 90, 90);
+    let mut hero = Fighter::new("Rustler", 12, 100, 100);
+    let mut monster = Fighter::new("Pythor", 15, 90, 90);
 
     println!("{:?}", hero);
     println!("{:?}", monster);
 
+    println!("\n");
+
     println!("{:?}", parse_action("attack"));
     println!("{:?}", parse_action("attack  "));
     println!("{:?}", parse_action("Attack"));
-    println!("{:?}", parse_action("Attzc zzk"));
-    println!("{:?}", parse_action("zd"));
-    println!("{:?}", parse_action(""));
+    // println!("{:?}", parse_action("Attzc zzk"));
+    // println!("{:?}", parse_action("zd"));
+    // println!("{:?}", parse_action(""));
+
+    println!("\n");
+
+    println!("{:?}", 
+    apply_action(&mut hero, &mut monster, Action::Attack));
+
+    println!("He has left {:?}HP", monster.current_hp);
+
+    println!("\n");
+    monster.take_damage(hero.attack_power.into());
 }
